cf7d42503bcc343b1b3cb9b71c7f2125b585adc7
Kang <caokang0720@gmail.com>
2026-05-30 20:45:18 -0400

    feat: updated parser for new web interface

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
 import csv
 import datetime as dt
+import logging
 import re
+import traceback
 import zoneinfo
 from typing import Any
 
@@ -11,6 +13,9 @@ from httpx import AsyncClient
 from oauth2client.service_account import ServiceAccountCredentials  # pyright: ignore[reportMissingTypeStubs]
 from pydantic import BaseModel, NonNegativeInt, PositiveInt, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+logger = logging.getLogger("main")
+logger.setLevel(logging.INFO)
 
 
 class ProxyConfig(BaseModel):
@@ -74,31 +79,26 @@ class Unit(BaseModel):
     @classmethod
     def date(cls, value: str):
         if value != "Now":
-            value = dt.datetime.strptime(value, "%m/%d/%y").strftime("%m/%d/%Y")
+            value = dt.datetime.strptime(value, "%m/%d/%Y").strftime("%m/%d/%Y")
         return value
 
 
-def extract_unit_info(raw: str) -> Unit:
-    print(f"Processing: {raw}")
-    pattern = (
-        r'Residence (?P<unit>\d+) in '
-        r'(?P<building>.+?) on '
-        r'(?P<address>[^,]+), '
-        r'(?P<bedroom>Studio|\d+ Bedroom(?:s)?) '
-        r'(?P<bathroom>\d+ Bathroom(?:s)?), '
-        r'(?P<sqft>[\d,]+) square feet, '
-        r'\$(?P<price>[\d,]+), '
-        r'Available (?P<available>Now|\d{1,2}/\d{1,2}/\d{2,4})')
-    match = re.search(pattern, raw)
+def extract_unit_info(ele: lxml.html.HtmlElement) -> Unit:
+
+    # e.g. 2701 at Newport Rentals on 40 Newport Parkway, Studio, 1 Bathroom, 492 square feet, Available 6/6/2026
+
+    pattern = r'(?P<unit>\d+) at Newport Rentals on (?P<address>[^,]+), (?P<bedroom>Studio|\d+ Bedroom(?:s)?), (?P<bathroom>\d+ Bathroom(?:s)?), (?P<sqft>[\d,]+) square feet, Available (?P<available>Now|\d{1,2}/\d{1,2}/\d{2,4})'
+
+    match = re.search(pattern, str(ele.attrib['aria-label']))
     assert match
     return Unit(
-        building_name=match.group("building"),
+        building_name=ele.cssselect('.availabilitylistings__building-name')[0].text.strip(),
         building_address=match.group("address"),
-        apartment_number=int(match.group("unit")),
-        num_bedroom=match.group("bedroom"),  # pyright: ignore[reportArgumentType]
-        num_bathroom=match.group("bathroom"),  # pyright: ignore[reportArgumentType]
-        square_footage=to_int(match.group("sqft")),
-        price=to_int(match.group("price")),
+        apartment_number=ele.cssselect('.availabilitylistings__residence-name')[0].text.strip().replace('Residence ', ''),
+        num_bedroom=match.group("bedroom"),
+        num_bathroom=match.group("bathroom"),
+        square_footage=match.group("sqft"),
+        price=to_int(ele.cssselect('[class="availabilitylistings__column availabilitylistings__column--rent"] [data-total-price]')[0].text.strip()),
         availability=match.group("available"),
     )
 
@@ -142,39 +142,21 @@ async def main():
 
     async with AsyncClient(
         headers={},  # TODO
+        follow_redirects=True,
         **kwargs
     ) as client:
-        await client.get("https://www.newportrentals.com/apartments-jersey-city-for-rent/")
+        resp = await client.get("https://www.newportrentals.com/apartments-jersey-city-for-rent")
 
-        page_num = 1
-        while True:
-            await asyncio.sleep(2)
-            resp = await client.post(
-                "https://www.newportrentals.com/ajax/getunitlist.asp",
-                data={
-                    "bedrooms": "",
-                    "priceMin":	1000,
-                    "isDefaultMinPrice": True,
-                    "priceMax": 20000,
-                    "isDefaultMaxPrice": True,
-                    "buildings": "",
-                    "moveInDate": "",
-                    "availableNowOnly": 0,
-                    "page": page_num,
-                    "lastNum": "",
-                    "sort": "",
-                    "numberPerPage": "undefined"
-                }
-            )
-
-            page_num += 1
-
-            if not resp.text:
-                break
-
-            html = lxml.html.fromstring(resp.text)  # pyright: ignore[reportUnknownMemberType]
-            for raw_string in html.xpath('//div[contains(@class, "unit-list-item")]//button/@aria-label'):
-                units.append(extract_unit_info(raw_string))
+        html = lxml.html.fromstring(resp.text)  # pyright: ignore[reportUnknownMemberType, reportUnknownVariableType]
+        locator = (
+            'div[class="availabilitylistings__row-container availabilitylistings__row-container--unit"][test] '
+            'button[class="availabilitylistings__row availabilitylistings__row--unit availabilitylistings__unit"]'
+        )
+        for ele in html.cssselect(locator):
+            try:
+                units.append(extract_unit_info(ele))
+            except Exception:
+                logger.error(f"Failed to parse unit: {ele.attrib.get('aria-label', None)}, {traceback.format_exc()=}")
 
         if SETTINGS.local_csv.save:
             write_csv(units)
